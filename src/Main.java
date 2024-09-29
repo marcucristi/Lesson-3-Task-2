eeb0a26ff5c146940e4a52f7b5c7ea55f43cc6f6
marcucristi <marcu.cristi19@gmail.com>
2024-09-29 20:45:32 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+// introducem valorile statisticei
+        long cWP =  7811080815L;
+        int cPtY =   55235358;
+        char fLiA = 'A';
+        boolean JiA = true;
+        double pI =  3.14159;
+// axam valorea necesara , statisticei necesare
+        System.out.println("Current World Population - " + cWP);
+        System.out.println("Cars Produced This Year - " + cPtY);
+        System.out.println("First Letter In Alphabet = " + fLiA);
+        System.out.println("Java Is Amazing - " + JiA);
+        System.out.println("Pi - " + pI);
+
+
+    }
+}
